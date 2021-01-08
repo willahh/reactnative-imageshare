99db6b2d8ce9ee6c279389d2acbc10eebf88d30d
wravel <wravel@gmail.com>
2021-01-09 00:46:40 +0100

    Add component TouchableOpacity

--- screens/TabThreeScreen.tsx
+++ screens/TabThreeScreen.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import { Image, StyleSheet } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
@@ -7,10 +9,20 @@ import { Text, View } from '../components/Themed';
 export default function TabThreeScreen() {
   return (
     <View style={styles.container}>
-      <Text style={{color: '#888', fontSize: 18}}>To share a photo from your phone with a friend, just press the button below!</Text>
+      <Image source={{uri: "https://i.imgur.com/TkIrScD.png"}} style={styles.logo}></Image>
+      <Text style={styles.instructions}>To 223share a photo from your phone with a friend, just press the button below!</Text>
+
+      <TouchableOpacity
+        onPress={() => alert('hello world')}
+        style={{ backgroundColor: '#999' }}
+        >
+          <Text style={{ fontSize: 20, color: '#fff'}}>Pick a photo</Text>
+      </TouchableOpacity>
     </View>
   );
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
@@ -27,4 +39,14 @@ const styles = StyleSheet.create({
     height: 1,
     width: '80%',
   },
+  logo: {
+    width: 305,
+    height: 195,
+    marginBottom: 10,
+  },
+  instructions: {
+    color: '#888',
+    fontSize: 18,
+    marginHorizontal: 15,
+  }
 });
